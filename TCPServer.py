c408acb95f88821e701449503936c9e9180f064f
devanharikumar89 <devanharikumar89@gmail.com>
2015-10-28 03:47:33 -0400

    changed the ip address

--- TCPServer.py
+++ TCPServer.py
@@ -2,11 +2,13 @@ from socket import *
 
 serverPort = 12000
 serverSocket = socket(AF_INET,SOCK_STREAM)
-serverSocket.bind(("localhost",serverPort))
+#In the line below give the local IP address of
+#the machine if the clients and server are in the same network.
+serverSocket.bind(("192.168.1.9",serverPort))
 serverSocket.listen(1)
 print("The server is listening")
 while 1:
     connectionSocket, addr = serverSocket.accept()
     sentence = connectionSocket.recv(1024)
     print(sentence)
-connectionSocket.close()
+    connectionSocket.close()
